d8327e84f37514db538fd7f0c269ad7eb58ec18f
virgantara <vinux.edu@gmail.com>
2022-08-16 16:40:33 +0700

    fix

--- elm.py
+++ elm.py
@@ -8,15 +8,21 @@ from sklearn.model_selection import train_test_split
 enc = preprocessing.OneHotEncoder(handle_unknown='ignore')
 
 from ELM import ELM
-iris = load_iris()
 
-X = iris.data
-y = iris.target.reshape(-1, 1)
+data = pd.read_csv("dataset/iris.csv")
+
+y = data['variety'].astype("category")
+
+
+X = data.drop('variety',axis=1)
+y = np.array(y).reshape(-1,1)
 y_encoded = enc.fit_transform(y).toarray().astype(np.float32)
-n_classes = 3
+n_classes = y_encoded.shape[1]
+
 normalized_X = preprocessing.normalize(X)
 
-x_train, x_test, t_train, t_test = train_test_split(normalized_X, y_encoded, test_size=0.25, random_state=42, shuffle=True)
+x_train, x_test, t_train, t_test = train_test_split(normalized_X, y_encoded, test_size=0.25, random_state=42,
+                                                    shuffle=True)
 
 model = ELM(
     jumlah_input_nodes=X.shape[1],
@@ -27,5 +33,5 @@ model = ELM(
 model.fit(x_train, t_train)
 loss, acc = model.evaluate(x_test, t_test)
 
-print("Loss : ",loss)
-print("Accuracy : ",acc)
+print("Loss : ", loss)
+print("Accuracy : ", acc)
